2263851610ee2dac32e7b34834088b2f4dace9b8
Marius Butuc <marius.butuc@gmail.com>
2013-05-17 19:50:43 -0400

    Initial Commit

---
+++ Hello Time/IAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IAAppDelegate.h
+//  Hello Time
+//
+//  Created by Marius Butuc on 13-05-17.
+//  Copyright (c) 2013 Marius Butuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

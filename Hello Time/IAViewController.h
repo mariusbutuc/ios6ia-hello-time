bb0d581c0d75499232c98520734aab2903cfca8e
Marius Butuc <marius.butuc@gmail.com>
2013-05-21 21:19:18 -0400

    First stab at iOS

--- Hello Time/IAViewController.h
+++ Hello Time/IAViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface IAViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
 
 @end
--- Hello Time/IAViewController.m
+++ Hello Time/IAViewController.m
@@ -17,13 +17,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[self checkTime:self];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)checkTime:(id)sender
+{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"h:mm:ss a"];
+    [self.timeLabel setText:[formatter stringFromDate:[NSDate date]]];
+    
+    [self performSelector:@selector(checkTime:) withObject:self afterDelay:1.0];
 }
 
 @end
